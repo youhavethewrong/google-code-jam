92db8956ce09845d5cb5fc9510e11f33d1150adf
Erick Crager <erick.crager@gmail.com>
2013-04-21 21:30:31 -0500

    have a solution for both large and small.  large takes about 1.5 min

--- africa-2010/credit.py
+++ africa-2010/credit.py
@@ -5,26 +5,27 @@
 
 import sys
 
-def check_pair(tot, item, iteml):
-    for i in iteml:
-        if item + i == tot:
+def check_pair(tot, index, iteml):
+    for i in range(len(iteml)):
+        #print "Checking if %s + %s = %s" % (item, i, tot)
+        if (index != i and int(iteml[index]) + int(iteml[i]) == int(tot)):
             return i
     return -1
 
 def find_match(credit, itemc, iteml):
     for i in range(itemc):
-        res = check_pair(credit, iteml[i], iteml)
+        res = check_pair(credit, i, iteml)
         if res > 0:
-            return res,iteml[i]
+            # 1-based indexing... really
+            return i+1,res+1
     return -1,-1
 
 fh = open(sys.argv[1], 'r')
 case_count = int(fh.readline())
 case_info = {}
 for i in range(case_count):
-    case_info[i] = {'credit':int(fh.readline()), 'itemc':int(fh.readline()), 'iteml':fh.readline()}
-print case_info
+    case_info[i] = {'credit':int(fh.readline()), 'itemc':int(fh.readline()), 'iteml':fh.readline()[:-1]}
 
 for i in range(len(case_info)):
     case = case_info[i]
-    print "Case #"+str(i)+": "+"%s %s" % find_match(case['credit'], case['itemc'], case['iteml'])
+    print "Case #"+str(i+1)+": "+"%s %s" % find_match(case['credit'], case['itemc'], case['iteml'].split(' '))
