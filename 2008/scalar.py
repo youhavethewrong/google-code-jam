4b44e7cf4f28c33d4468ea06dcea96561534f446
Erick Crager <erick.crager@gmail.com>
2013-05-01 22:55:21 -0500

    broken...

--- 2008/scalar.py
+++ 2008/scalar.py
@@ -10,7 +10,15 @@ def scalarProduct(l, v1, v2):
         print "%s * %s" % (v1[i], v2[i])
         product += int(v1[i]) * int(v2[i])
     return product
-        
+
+def permute(factor, vector):
+    return [i * factor for i in vector]
+
+def twiddle(i, j, v1, v2):
+    return scalarProduct(l, permute(i, v1), permute(j, v2))
+
+def minimize(l, v1, v2):
+    
 if __name__ == '__main__':
     fh = open(sys.argv[1], 'r')
     cases = int(fh.readline())
@@ -22,4 +30,4 @@ if __name__ == '__main__':
 
     for case in case_info:
         info = case_info[case]
-        print scalarProduct(info['len'], info['x'], info['y'])
+        print minimize(info['len'], info['x'], info['y'])
