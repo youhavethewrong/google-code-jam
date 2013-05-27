12e7111ec7e569cad74d6eeb5b3a53caa71a4d49
Negated <youhavethewrong@gmail.com>
2013-05-27 12:31:33 -0500

    MSP.groovy works for sample and small, but is judged incorrect for large. discover why.

--- groovy/2008/MinimumScalarProduct.groovy
+++ groovy/2008/MinimumScalarProduct.groovy
@@ -3,20 +3,34 @@
  */
 
 def minimize(len, v1, v2) {
-    return 42
+    def sum = 0
+    v1 = v1.sort()
+    v2 = v2.sort().reverse()
+    for (def i=0; i < v1.size(); i++) {
+        sum += v1[i] * v2[i]
+    }
+    return sum
 }
 
 def main() {
-    fh = new File(args[0])
-    lines = fh.readLines()
-    caseCount = Integer.valueOf(lines[0])
+    def fh = new File(args[0])
+    def lines = fh.readLines()
+    def caseCount = Integer.valueOf(lines[0])
     def cases = [:]
-    for(i=1; i <= caseCount; i++) {
-        index = 3*(i-1)+1
-        cases[(i)] = [len:lines[index], v1:lines[index+1].split(" "), v2:lines[index+2].split(" ")]
+
+    for(def i=1; i <= caseCount; i++) {
+        def index = 3*(i-1)+1
+
+        def v1 = lines[index+1].split(" ")
+        v1 = v1.collect { Integer.valueOf(it) }
+
+        def v2 = lines[index+2].split(" ")
+        v2 = v2.collect { Integer.valueOf(it) }
+
+        cases[(i)] = [len:lines[index], v1:v1, v2:v2]
     }
 
-    for( j in cases.keySet() ) {
+    for( def j in cases.keySet() ) {
         def caseRun = cases[j]
         println "Case #$j: "+minimize(caseRun['len'], caseRun['v1'], caseRun['v2'])
     }
